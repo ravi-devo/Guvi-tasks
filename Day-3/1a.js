301242b3a597ab1f1068c542c9bb4455c0053dce
ravi-devo <ravi.ravichandran370@gmail.com>
2023-07-13 20:24:36 +0530

    Print odd numbers from an array

--- Day-3/1a.js
+++ Day-3/1a.js
@@ -1,2 +1,13 @@
 // Print odd numbers of an array
 let userInput = [1, 2, 3, 4, 5, 6];
+let answerArray = [];
+
+(function (userInput){
+    let arr = userInput;
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] % 2 == 1){
+            answerArray.push(arr[i]);
+        }
+    }
+    console.log(answerArray.join(" "));
+})(userInput);
