4ab5b1bfeafbe51b729874fbafbfbc11a1646e3d
ravi-devo <ravi.ravichandran370@gmail.com>
2023-09-06 20:45:28 +0530

    callback hell

---
+++ Day-15/script.js
@@ -0,0 +1,33 @@
+let timer = 10;
+let time = document.getElementById("displayId");
+setTimeout(() => {
+    time.innerHTML = `<p>${timer--}</p>`;
+    setTimeout(() => {
+        time.innerHTML = `<p>${timer--}</p>`;
+        setTimeout(() => {
+            time.innerHTML = `<p>${timer--}</p>`;
+            setTimeout(() => {
+                time.innerHTML = `<p>${timer--}</p>`;
+                setTimeout(() => {
+                    time.innerHTML = `<p>${timer--}</p>`;
+                    setTimeout(() => {
+                        time.innerHTML = `<p>${timer--}</p>`;
+                        setTimeout(() => {
+                            time.innerHTML = `<p>${timer--}</p>`;
+                            setTimeout(() => {
+                                time.innerHTML = `<p>${timer--}</p>`;
+                                setTimeout(() => {
+                                    time.innerHTML = `<p>${timer--}</p>`;
+                                    setTimeout(() => {
+                                        time.innerHTML = `<p>${timer--}</p>`;
+                                        time.innerHTML = `<p>Happy Independence Day!`
+                                    }, 1000)
+                                }, 1000)
+                            }, 1000)
+                        }, 1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}, 1000)
